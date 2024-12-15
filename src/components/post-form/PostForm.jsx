e80cd3999173b36ea343b0df82accc0b54e0ee29
Shivesh Gupta <139038761+guptashivesh123@users.noreply.github.com>
2024-12-16 00:07:22 +0530

    Update PostForm.jsx

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -28,17 +28,17 @@ export default function PostForm({ post }) {
 
             const dbPost = await appwriteService.updatePost(post?.$id, {
                 ...data,
-                featuredImage: file ? file.$id : undefined,
+                featuredImage: file ? file?.$id : undefined,
             });
 
             if (dbPost) {
-                navigate(`/post/${dbPost.$id}`);
+                navigate(`/post/${dbPost?.$id}`);
             }
         } else {
             const file = await appwriteService.uploadFile(data.image[0]);
 
             if (file) {
-                const fileId = file.$id;
+                const fileId = file?.$id;
                 data.featuredImage = fileId;
                 const dbPost = await appwriteService.createPost({ ...data, userId: userData?.$id });
 
